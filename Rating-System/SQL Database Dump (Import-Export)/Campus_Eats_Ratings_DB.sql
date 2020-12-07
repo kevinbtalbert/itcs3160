813291f863bf00eb610fd355db20b56ef85ba870
jorgeortiz710 <48526576+jorgeortiz710@users.noreply.github.com>
2020-12-06 22:54:40 -0500

    Update Campus_Eats_Ratings_DB.sql
    This is the updated campus_eats_ratings db with relationships

--- Rating-System/SQL Database Dump (Import-Export)/Campus_Eats_Ratings_DB.sql
+++ Rating-System/SQL Database Dump (Import-Export)/Campus_Eats_Ratings_DB.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.0.21, for macos10.15 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.21, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: Campus_Eats_Fall2020
+-- Host: localhost    Database: campus_eats_fall2020
 -- ------------------------------------------------------
--- Server version	8.0.22
+-- Server version	8.0.21
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -285,10 +285,6 @@ LOCK TABLES `restaurant` WRITE;
 UNLOCK TABLES;
 
 --
--- Dumping routines for database 'Campus_Eats_Fall2020'
---
-
---
 -- Final view structure for view `person_join`
 --
 
@@ -333,4 +329,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-12-04 17:58:05
+-- Dump completed on 2020-12-06 22:42:43
